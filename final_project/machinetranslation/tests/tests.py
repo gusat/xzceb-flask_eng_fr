321e8abbd396cdf25858eeee0be3a1e80170b7d7
gusat <mgusat@gmail.com>
2023-05-17 11:07:12 +0200

    Update tests.py
    added both eq and noteq asserts

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -2,18 +2,22 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TestTranslator(unittest.TestCase):
-
+    
     def test_englishToFrench_translation(self):
         english_text = 'Hello'
         expected_result = 'Bonjour'
+        unexpected_result = 'Hello'
         result = english_to_french(english_text)
         self.assertEqual(result, expected_result)
+        self.assertNotEqual(result, unexpected_result)
 
     def test_frenchToEnglish_translation(self):
         french_text = 'Bonjour'
         expected_result = 'Hello'
+        unexpected_result = 'Bonjour'
         result = french_to_english(french_text)
         self.assertEqual(result, expected_result)
+        self.assertNotEqual(result, unexpected_result)
 
     def test_englishToFrench_null_input(self):
         english_text = None
